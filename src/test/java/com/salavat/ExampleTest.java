30401a93d11298fb8c1e163da208f8aede15b45b
SalavatYuldashev <salavat.yuldashev@vk.com>
2025-05-26 23:38:26 +0300

    initial commit

---
+++ src/test/java/com/salavat/ExampleTest.java
@@ -0,0 +1,10 @@
+package com.salavat;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("Тест работает!");
+    }
+}
